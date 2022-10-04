41060a68cbf69e9cc6d52dea573a4d50fde19472
Andrew Titov <andtit2001@gmail.com>
2022-10-04 10:47:10 +0300

    Fix constraint

--- task1/type_tuples.hpp
+++ task1/type_tuples.hpp
@@ -8,6 +8,6 @@ template<class... Ts>
 struct TTuple {};
 
 template<class TT>
-concept TypeTuple = requires(TT t) { []<class... Ts>(TTuple<Ts...>){}(t) };
+concept TypeTuple = requires(TT t) { []<class... Ts>(TTuple<Ts...>){}(t); };
 
 } // namespace type_tuples

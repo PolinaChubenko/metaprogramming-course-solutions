711470e82474ed3b0cd8cd1b9dba3ecbfb82bcd3
Polina Chubenko <chubenko.pn@phystech.edu>
2023-11-17 00:44:03 +0300

    done

--- task4/FixedString.hpp
+++ task4/FixedString.hpp
@@ -1,12 +1,26 @@
 #pragma once
 
+#include <algorithm>
+#include <cinttypes>
+#include <cstring>
+#include <string_view>
 
-template<size_t max_length>
+
+template<std::size_t max_length>
 struct FixedString {
-  FixedString(const char* string, size_t length);
-  operator std::string_view() const;
+  constexpr FixedString(const char* string, std::size_t length) : length(length) {
+    std::copy(string, string + length, storage);
+    std::fill(storage + length, storage + max_length, '\0');
+  }
 
-  // std::string impl; ???
+  constexpr operator std::string_view() const {
+    return std::string_view(storage, length);
+  }
+
+  char storage[max_length];
+  std::size_t length;
 };
 
-// operator ""_cstr ?
+constexpr FixedString<256> operator ""_cstr(const char* data, std::size_t size) {
+  return FixedString<256>(data, size);
+}
--- task4/PolymorphicMapper.hpp
+++ task4/PolymorphicMapper.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
 #include <optional>
+#include <typeinfo>
 
 
 template <class From, auto target>
-struct Mapping;
+struct Mapping {};
 
 template <class Base, class Target, class... Mappings>
 struct PolymorphicMapper {
-  static std::optional<Target> map(const Base& object);
+  static std::optional<Target> map(const Base& object) {
+    std::optional<Target> opt;
+    (
+      [&object, &opt]<typename From, Target target>(Mapping<From, target>) {
+        try {
+          [[maybe_unused]] const auto& unused = dynamic_cast<const From&>(object);
+          if (!opt.has_value()) {
+            opt = target;
+          }
+        } catch (const std::bad_cast&) {}
+      }(Mappings{})
+      , ...
+    );
+    return opt;
+  }
 };
